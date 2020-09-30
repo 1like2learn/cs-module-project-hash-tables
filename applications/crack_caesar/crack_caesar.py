70374643374c43b8b1455bc38a5faea45f9eef64
1like2learn <ndespained@gmail.com>
2020-09-30 15:38:18 -0700

    crack casesar done

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -2,4 +2,44 @@
 # decode it.
 
 # Your code here
+# charFilter = {':',';',',','.','-','+','=','/','\\','|','[',']','{','}','(',')','*','^','&','\"'}
+charFilter = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'}
 
+with open("ciphertext.txt") as f:
+  words = f.read()
+
+letterCache = {}
+totalNumChar = 0
+
+for char in words:
+  if char in charFilter:
+    totalNumChar += 1
+    if char in letterCache:
+      letterCache[char] += 1
+    else:
+      letterCache[char] = 1
+
+charFrequency = {}
+for key in letterCache:
+  charFrequency[key] = letterCache[key] / totalNumChar
+
+charFrequencyList = list(charFrequency.items())
+charFrequencyList.sort(key = lambda t: t[1], reverse = True)
+
+decipher = {}
+lettersByFrequency = ['E', 'T', 'A', 'O', 'H', 'N', 'R', 'I', 'S', 'D', 'L', 'W', 'U', 'G', 'F', 'B', 'M', 'Y', 'C', 'P', 'K', 'V', 'Q', 'J', 'X', 'Z']
+print(lettersByFrequency)
+print(charFrequencyList)
+for i, touple in enumerate(charFrequencyList):
+  decipher[touple[0]] = lettersByFrequency[i]
+print(decipher)
+
+decipheredWords = ""
+for char in words:
+  if char in decipher:
+    decipheredWords += decipher[char]
+  else:
+    decipheredWords += char
+
+
+print(decipheredWords)
--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -10,21 +10,32 @@ wordsArr = words.split()
 startWords = []
 nextWords = {}
 endingPunctuation = {".", "?","!"}
+# Loop through all the words in wordsArr
 for i,word in enumerate(wordsArr):
+    # If the word has an uppercase char add it to the list of start words
     if not word.islower():
         startWords.append(word)
+    # If the word is a key in nextWords add the word in the next index to 
+    # the word's list of next words
     if word in nextWords:
         nextWords[word].append(wordsArr[i + 1])
+    # If the word is not a key make it one and create an array with it's 
+    # next word as the only entry
     else:
+        # Check to make sure we aren't at the end of the input
         if i + 1 < len(wordsArr):
             nextWords[word] = [wordsArr[i + 1]]
 
 # TODO: construct 5 random sentences
 # Your code here
 def constructSentence():
+    # Our output and current word are randomly taken from startWords
     output = word = random.choice(startWords)
+    # Loop while the length of the word is less than one or word is an end word
     while len(word) < 1 or (not word[-1] in endingPunctuation and not word[-2] in endingPunctuation):
+        # Make word the next word from nextWords
         word = random.choice(nextWords[word])
+        # Add the next word to the output
         output += " " + word
     return output
 print(constructSentence())
